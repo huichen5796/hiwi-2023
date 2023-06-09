266d478c8ca8910cbb3c50ee5dbf6db5635d221f
Hui Chen <hchen5796@gmail.com>
2023-06-09 14:32:39 +0200

    Update onehot_transfer.py

--- timm2/my_timm/onehot_transfer.py
+++ timm2/my_timm/onehot_transfer.py
@@ -1,9 +1,12 @@
+import numpy as np
+
+print('dododododoodododood')
 def onehot_trans(label: float):
     if label == 1.0:
-        return [1.0, 0.0, 0.0, 0.0]
+        return np.array([1.0, 0.0, 0.0, 0.0])
     elif label == 0.0:
-        return [0.0, 1.0, 0.0, 0.0]
+        return np.array([0.0, 1.0, 0.0, 0.0])
     elif label == -1.0:
-        return [0.0, 0.0, 1.0, 0.0]
+        return np.array([0.0, 0.0, 1.0, 0.0])
     else:
-        return [0.0, 0.0, 0.0, 1.0]
+        return np.array([0.0, 0.0, 0.0, 1.0])
